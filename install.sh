f58077e6773fcc7f223716b099e521f6afda77fb
yaretsuo <yaretsuo@gmail.com>
2026-04-10 15:07:16 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 sudo cp -rf Bibata-Modern-Ice/ /usr/share/icons
 cp -rf dunst/ hypr/ kitty/ mpv/ qt5ct/ qt6ct/ rofi/ waybar/ yazi/ ~/.config
 mkdir -p ~/Pictures && cp -rf tux.png ~/Pictures && awww-daemon & swww img ~/Pictures/background.png
-sudo cp -rf yaretsuo-black-theme/ /usr/share/sddm/themes
+sudo cp -rf yaretsuo-monochrome-theme/ /usr/share/sddm/themes
 sudo cp -rf sddm.conf /etc/sddm.conf
 clear
 echo "It's highly recommended to reboot the PC"
