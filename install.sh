9a5f747651777797be4632718a4861925cfc2614
yaretsuo <yaretsuo@gmail.com>
2026-04-10 15:05:20 +0200

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,7 @@
+sudo cp -rf Bibata-Modern-Ice/ /usr/share/icons
+cp -rf dunst/ hypr/ kitty/ mpv/ qt5ct/ qt6ct/ rofi/ waybar/ yaretsuo-black-theme/ yazi/ ~/.config
+mkdir -p ~/Pictures && cp -rf tux.png ~/Pictures && awww-daemon & swww img ~/Pictures/background.png
+sudo cp -rf yaretsuo-black-theme/ /usr/share/sddm/themes
+sudo cp -rf sddm.conf /etc/sddm.conf
+clear
+echo "It's highly recommended to reboot the PC"
