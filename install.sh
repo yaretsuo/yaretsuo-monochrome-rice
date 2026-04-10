674f66f5744743e4698b0cc3f8186e0ae702543f
yaretsuo <yaretsuo@gmail.com>
2026-04-10 20:06:25 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 sudo cp -rf Bibata-Modern-Ice/ /usr/share/icons
 cp -rf dunst/ hypr/ kitty/ mpv/ qt5ct/ qt6ct/ rofi/ waybar/ yazi/ ~/.config
-mkdir -p ~/Pictures && cp -rf tux.png ~/Pictures && awww-daemon & swww img ~/Pictures/background.png
+mkdir -p ~/Pictures && cp -rf tux.png ~/Pictures && awww-daemon && swww img ~/Pictures/background.png
 sudo cp -rf yaretsuo-monochrome-theme/ /usr/share/sddm/themes
 sudo cp -rf sddm.conf /etc/sddm.conf
 clear
