1790940b6e1f2e3eed961c7d25cf3035da42ebd7
yaretsuo <yaretsuo@gmail.com>
2026-05-24 10:59:30 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,9 @@
-sudo cp -rf Bibata-Modern-Ice/ /usr/share/icons
-cp -rf dunst/ hypr/ kitty/ mpv/ qt5ct/ qt6ct/ rofi/ waybar/ yazi/ ~/.config
-mkdir -p ~/Pictures && cp -rf tux.png ~/Pictures
+sudo cp -rf Bibata-Modern-Ice/ /usr/share/icons/
+cp -rf dunst/ hypr/ kitty/ mpv/ qt5ct/ qt6ct/ rofi/ waybar/ yazi/ ~/.config/
+mkdir -p ~/Pictures/ && cp -rf tux.png ~/Pictures/
 awww-daemon && awww img ~/Pictures/tux.png
-sudo cp -rf yaretsuo-monochrome-theme/ /usr/share/sddm/themes
+sudo cp -rf yaretsuo-monochrome-theme/ /usr/share/sddm/themes/
 sudo cp -rf sddm.conf /etc/sddm.conf
+cp -rf default/ ~/.icons/
 clear
 echo "It's highly recommended to reboot the PC"
